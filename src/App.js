b6e1131344aa22812f9f3b33638a212d4b17126e
Falah-Ai <alhooz2000@gmail.com>
2025-12-01 06:54:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-```javascript
 import React from 'react';
-import VoiceAssistant from './components/VoiceAssistant/VoiceAssistant';
 import './App.css';
+import VoiceAssistant from './components/VoiceAssistant/VoiceAssistant';
 
 function App() {
   return (
     <div className="App">
+      <h1>Absher Voice Assistant</h1>
       <VoiceAssistant />
     </div>
   );
 }
 
-export default App
+export default App;
